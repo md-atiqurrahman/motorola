be8f22bf012877b9312b59ea0b4e64a8a96c1ae1
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2023-04-26 03:04:58 +0600

    start the project

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "animate.css";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <div>
+        <h1 className="animate__animated animate__zoomInDown">Hello Moto...</h1>
+        <h3>Have any curiosity?</h3>
+        <div className="contact">
+          Contact Us:
+          <p>atik247@yahoo.com</p>
+          <p>tanshir1112@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
